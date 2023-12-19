0774a96b26beca4a6b8eaab4de03666272a6b3f6
riccardo.tornesello <riccardo.tornesello@gmail.com>
2023-12-19 08:21:30 +0100

    delete old project

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Spinner } from '@chakra-ui/react';
-
-export default function HomeLoading() {
-  return (
-    <Center h='100vh' w='100%'>
-      <Spinner size='xl' />
-    </Center>
-  );
-}
--- features/auth/schemas/signin.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const signInSchema = z.object({
-  username: z.string().min(1, { message: 'Username or email is required' }),
-  password: z.string().min(1, { message: 'The password is required' }),
-});
-
-export type SignInSchema = z.infer<typeof signInSchema>;

b61c59358c6c412e5e2c3e43de878ed82f171fb5
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-01-01 22:24:04 +0100

    favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Play with ELO',
+  description: 'The platform to create elo-ranking based competitions!',
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -17,7 +17,12 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme={defaultColorScheme} />
-        <link rel="shortcut icon" href="/favicon.svg" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
